f207787d81d020fc970d29ffcaa352308db24954
shane325 <shane325@gmail.com>
2018-06-20 15:28:12 -0700

    Moved some variables into a seperate config file.

---
+++ src/config/news.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/**
+ * Module dependencies
+ */
+
+module.exports = {
+  NEWS_API_KEY: '811b23ddeeea4201a119835b0334d5c5',
+  query: 'email,security',
+  language: 'en'
+}
--- src/main.controller.js
+++ src/main.controller.js
@@ -4,21 +4,19 @@
  * Module dependencies
  */
 const NewsAPI = require('newsapi')
-const news = new NewsAPI('811b23ddeeea4201a119835b0334d5c5')
+let newsConfig = require('./config/news')
+const news = new NewsAPI(newsConfig.NEWS_API_KEY)
 
 /**
  * Return articles
  */
 module.exports.getArticles = (req, res) => {
   news.v2.everything({
-    q: 'bitcoin',
-    sources: 'bbc-news,the-verge',
-    domains: 'bbc.co.uk, techcrunch.com',
+    q: newsConfig.query,
     from: '2017-12-01',
     to: '2017-12-12',
-    language: 'en',
-    sortBy: 'relevancy',
-    page: 2
+    language: newsConfig.language,
+    page: 1
   }).then(response => {
     res.json(response)
   })
