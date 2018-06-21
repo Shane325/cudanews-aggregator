9e2b912e0a3c9bbd093d29b0506918e0c3d9aa90
shane325 <shane325@gmail.com>
2018-06-21 14:43:01 -0700

    fixed port

--- server.js
+++ server.js
@@ -5,10 +5,10 @@
  */
 let express = require('express')
 let app = express()
-let port = process.env.port || 8080
+let port = process.env.PORT || 8080
 let controller = require('./src/main.controller')
 
-setInterval(controller.getArticles, 1000 * 120) // 2 minute interval
+setInterval(controller.getArticles, 1000 * 90) // 1.5 minute interval
 
 app.listen(port)
 console.log('App listening on port ', port)
